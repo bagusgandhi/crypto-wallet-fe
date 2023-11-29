cbbaad636613d6887304ed5715e2964186c59a24
bagusgandhi <bagusgandhi4@gmail.com>
2023-11-29 23:45:33 +0700

    feat: adding affix on header for, set it sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Button } from "antd";
+import { Layout, Button, Affix } from "antd";
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
@@ -14,26 +14,28 @@ const Navbar: React.FC = () => {
     const { collapsed, setCollapsed } = useDashboardStore()
 
     return (
-        <Header
-            className="!bg-white !shadow-md  !px-4 !py-4"
-        >
-            <div className='flex justify-between items-center'>
-                <Button
-                    type="text"
-                    icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                    onClick={() => setCollapsed()}
-                />
+        <Affix offsetTop={0} className='z-30'>
+            <Header
+                className="!bg-white !px-4 !py-4  w-full z-20 border-b-2 border-black border-opacity-10"
+            >
+                <div className='flex justify-between items-center'>
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed()}
+                    />
 
-                <Button
-                    type="text"
-                    danger
-                    icon={<LogoutOutlined />}
-                    onClick={() => logout()}
-                >
-                    Logout
-                </Button>
-            </div>
-        </Header>
+                    <Button
+                        type="text"
+                        danger
+                        icon={<LogoutOutlined />}
+                        onClick={() => logout()}
+                    >
+                        Logout
+                    </Button>
+                </div>
+            </Header>
+        </Affix>
     )
 }
 
