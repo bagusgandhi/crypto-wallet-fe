c0cb518f00b6564025428e237624e76afab40599
bagusgandhi <bagusgandhi4@gmail.com>
2023-11-29 07:28:49 +0700

    feat: sidebar and menu navigation

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react';
+import { Layout, Menu, Affix } from "antd";
+import {
+    DashboardOutlined,
+    UserOutlined,
+    LineChartOutlined,
+    DollarOutlined
+} from '@ant-design/icons';
+import { useDashboardStore } from '../store/dashboard';
+import { Link } from 'react-router-dom';
+
+const { Sider } = Layout;
+
+const Sidebar: React.FC = () => {
+    const { collapsed, setSelectedMenu, selectedMenu } = useDashboardStore();
+
+    useEffect(() => {
+        console.log(selectedMenu)
+    }, [selectedMenu]);
+
+    return (
+        <Affix offsetTop={0}>
+            <Sider trigger={null} className="!h-screen !bg-slate-50 z-10" collapsible collapsed={collapsed}>
+                <div className="p-3 text-white text-lg bg-blue-400 justify-center m-3 rounded-md flex items-center gap-2">
+                    <DollarOutlined />
+                    {collapsed ? (<></>) : (<h3>Crypto Wallet</h3>)}
+                </div>
+                <Menu
+                    className="!bg-transparent !py-4 !px-2 !border-0 text-black !hover:text-indigo-100"
+                    defaultSelectedKeys={['dashboard']}
+                    selectedKeys={[selectedMenu]}
+                    theme="light"
+                    items={[
+                        {
+                            key: 'dashboard',
+                            icon: <DashboardOutlined />,
+                            label: (<>
+                                <Link to="/" onClick={() => setSelectedMenu("dashboard")}>
+                                    Dashboard
+                                </Link>
+                            </>),
+                        },
+                        {
+                            key: 'transactions',
+                            icon: <LineChartOutlined />,
+                            label: (<>
+                                <Link to="/transactions" onClick={() => setSelectedMenu("transactions")} >
+                                    Transactions
+                                </Link>
+                            </>),
+                        },
+                        {
+                            key: 'user',
+                            icon: <UserOutlined />,
+                            label: (<>
+                                <Link to="/users" onClick={() => setSelectedMenu("user")} >
+                                    Users
+                                </Link>
+                            </>),
+                        },
+                    ]}
+                />
+            </Sider>
+        </Affix>
+    )
+}
+
+export default Sidebar;
