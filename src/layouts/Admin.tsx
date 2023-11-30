d616ab3dae6af185d267854744222412e11c00ec
bagusgandhi <bagusgandhi4@gmail.com>
2023-12-01 00:47:33 +0700

    feat: add Toast component

--- src/layouts/Admin.tsx
+++ src/layouts/Admin.tsx
@@ -2,16 +2,18 @@ import React, { ReactNode } from 'react';
 import { Layout } from 'antd';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
+import { Toaster } from 'react-hot-toast';
 
 const { Content } = Layout;
 
-const AdminLayout: React.FC<{children: ReactNode}> = ({ children }) => {
+const AdminLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
     return (
         <>
+            <Toaster />
             <Layout>
                 <Sidebar />
                 <Layout>
-                <Navbar />
+                    <Navbar />
                     <Content className="!w-full bg-white">
                         <div>
                             {children}
