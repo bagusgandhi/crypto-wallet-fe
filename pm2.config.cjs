cb87d8c3cf430bf4096fe757536355cf276ac392
bagusgandhi <bagusgandhi4@gmail.com>
2023-11-30 15:16:05 +0700

    feat: create pm2 config

---
+++ pm2.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'crypto-wallet',
+    script: 'serve',
+    env: {
+      PM2_SERVE_PATH: './dist',
+      PM2_SERVE_PORT: 3000,
+      PM2_SERVE_SPA: 'true',
+      PM2_SERVE_HOMEPAGE: '/index.html'
+    }
+  };
+  
+  
