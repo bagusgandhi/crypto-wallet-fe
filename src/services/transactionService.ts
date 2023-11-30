a37b35020f2ca42d3a62f151c4aa584f953e46cd
bagusgandhi <bagusgandhi4@gmail.com>
2023-12-01 00:50:10 +0700

    feat: adding data fetching for report and log by user

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -7,20 +7,36 @@ interface TransactionParams {
     page?: number;
     limit?: number;
     user_id?: string;
-    transaction_type: string | null;
-    from: string | null;
-    to: string | null;
-  }
+    transaction_type?: string | null;
+    from?: string | null;
+    to?: string | null;
+}
 
 export const useTransactionReport = async (params: TransactionParams) => {
     try {
         const token = Cookies.get('token');
         const response = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}transactions/report`, {
             headers: { Authorization: `Bearer ${token}` },
-            params: { ...params}
+            params: { ...params }
         });
 
         useTransactionStore.getState().setSeries(response.data.series);
+
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response.data.message)
+    }
+}
+
+export const useTransactionReportByUser = async (params: TransactionParams) => {
+    try {
+        const token = Cookies.get('token');
+        const response = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}transactions/report`, {
+            headers: { Authorization: `Bearer ${token}` },
+            params: { ...params }
+        });
+
+        useTransactionStore.getState().setSeriesByUser(response.data.series);
 
         return response.data;
     } catch (error: any) {
@@ -33,11 +49,29 @@ export const useTransactionLog = async (params: TransactionParams) => {
         const token = Cookies.get('token');
         const response = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}transactions/log`, {
             headers: { Authorization: `Bearer ${token}` },
-            params: { ...params}
+            params: { ...params }
         });
 
         useTransactionStore.getState().setLog(response.data.data);
         useTransactionStore.getState().setTotalLogPage(response.data.total_pages);
+
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response.data.message)
+    }
+}
+
+export const useTransactionLogByUser = async (params: TransactionParams) => {
+    try {
+        const token = Cookies.get('token');
+        const response = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}transactions/log`, {
+            headers: { Authorization: `Bearer ${token}` },
+            params: { ...params }
+        });
+
+        useTransactionStore.getState().setLogByUser(response.data.data);
+
+        useTransactionStore.getState().setTotalLogPageByUser(response.data.total_pages);
 
         return response.data;
     } catch (error: any) {
